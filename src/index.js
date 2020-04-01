40ca681ea8715564b1c6364bc6e608597da740cd
shidongyang <851051279@qq.com>
2020-04-01 23:47:42 +0800

    安装好react react-dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('aaaaaa111')
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Taylor" />,
+  document.getElementById('app')
+)
