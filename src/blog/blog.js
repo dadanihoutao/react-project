fc5de7fdb9d7a12639d17e4b3845262999b73dfe
shidongyang <851051279@qq.com>
2020-04-04 15:02:30 +0800

    使用react-router-dom 搭建一个简单的有路由的页面

--- src/blog/blog.js
+++ src/blog/blog.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'
 
-export default class Home extends Component {
+export default class Blog extends Component {
   render () {
     return (
       <div className="test test2">
-        <p>home pages</p>
+        <p>blog pages</p>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import ReactDom from 'react-dom'
-import HomePage from '@/home/home.js'
+// import HomePage from '@/home/home.js'
+import AppRouter from '@/router/router'
 
 class App extends Component {
   render() {
     return (
-      <div style={{color: "#333"}}>
-        <HomePage/>
+      <div>
+        <AppRouter/>
       </div>
     )
   }
