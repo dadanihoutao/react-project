de66b460aec475648e55df29041eff28aef484b2
shidongyang <851051279@qq.com>
2020-04-06 23:07:11 +0800

    整理配置代码格式

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -18,15 +18,15 @@ const webpackConfig = webpackMerge(baseWebpackConfig, {
         // 打包前清理dist文件插件,不用在实例中传入路径参数，默认清理dist 文件夹
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-        filename: utils.resolve('../dist/index.html'), // html模板的生成路径
-        template: 'index.html', // html模板文件
-        inject: true, // true：默认值，script标签位于html文件的 body 底部
-        hash: true, // 在打包的资源插入html后会加上hash
-        minify: { // html 文件进行压缩
-            removeComments: true,               //去注释
-            collapseWhitespace: true,           //压缩空格
-            removeAttributeQuotes: true         //去除属性 标签的 引号  例如 <p id="test" /> 输出 <p id=test/>
-        }
+            filename: utils.resolve('../dist/index.html'), // html模板的生成路径
+            template: 'index.html', // html模板文件
+            inject: true, // true：默认值，script标签位于html文件的 body 底部
+            hash: true, // 在打包的资源插入html后会加上hash
+            minify: { // html 文件进行压缩
+                removeComments: true,               //去注释
+                collapseWhitespace: true,           //压缩空格
+                removeAttributeQuotes: true         //去除属性 标签的 引号  例如 <p id="test" /> 输出 <p id=test/>
+            }
         })
     ],
     // 代码压缩配置 （css,js压缩）
@@ -99,8 +99,11 @@ const webpackConfig = webpackMerge(baseWebpackConfig, {
                     reuseExistingChunk: true
                 }
             }
+        },
+        runtimeChunk:{
+            name:'manifest' // webpack的运行文件
         }
-    }
+    }å
 })
 if (process.env.npm_config_report) {
     // 打包分析插件
