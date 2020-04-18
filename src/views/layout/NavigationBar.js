c6be8b72cedd15417a9822b7bfd5233f9062a05b
shidongyang <851051279@qq.com>
2020-04-18 13:30:32 +0800

    修改头像

--- src/views/layout/NavigationBar.js
+++ src/views/layout/NavigationBar.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import headImg from '@/assets/images/yeji.jpg'
 
 export default class NavigationBar extends Component {
     render () {
         return (
             <div class="navigation-bar">
                 <div class="nav-logo">
-                    <img class="logo-img" src="http://upload.jianshu.io/users/upload_avatars/2152694/e1c9a0f8-fd43-4c51-a432-270b04da7e48.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240"/>
+                    <img class="logo-img" src={headImg}/>
                     <p class="title">后台管理系统</p>
                 </div>
                 <div class="nav-content">
